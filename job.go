9e5dfc73722a93f2b870b272100e6a4fe2aba701
pchchv <ipchchv@gmail.com>
2023-12-13 11:27:45 +0300

    Add a Q type

---
+++ job.go
@@ -0,0 +1,5 @@
+package work
+
+// Q is a shortcut to easily specify arguments for jobs when enqueueing them.
+// Example: e.Enqueue("send_email", work.Q{"addr": "test@example.com", "track": true})
+type Q map[string]interface{}
