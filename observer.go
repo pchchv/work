51fcc837b8159c4bf4dbbf44c51d209830389196
pchchv <ipchchv@gmail.com>
2023-12-13 11:25:14 +0300

    Add an observation type

--- observer.go
+++ observer.go
@@ -1,3 +1,18 @@
 package work
 
 type observationKind int
+
+type observation struct {
+	kind observationKind
+	// These fields always need to be set
+	jobName string
+	jobID   string
+	// These need to be set when starting a job
+	startedAt int64
+	arguments map[string]interface{}
+	// If we're done w/ the job, err will indicate the success/failure of it
+	err error // nil: success. not nil: the error we got when running the job
+	// If this is a checkin, set these.
+	checkin   string
+	checkinAt int64
+}
