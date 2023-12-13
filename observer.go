7addab1634d4b9e413641605651e7179e833f19b
pchchv <ipchchv@gmail.com>
2023-12-13 11:26:38 +0300

    Add an observer type

--- observer.go
+++ observer.go
@@ -1,5 +1,7 @@
 package work
 
+import "github.com/gomodule/redigo/redis"
+
 type observationKind int
 
 type observation struct {
@@ -15,4 +17,25 @@ type observation struct {
 	// If this is a checkin, set these.
 	checkin   string
 	checkinAt int64
+}
+
+// An observer observes a single worker. Each worker has its own observer.
+type observer struct {
+	namespace string
+	workerID  string
+	pool      *redis.Pool
+	// nil: worker isn't doing anything that we know of
+	// not nil: the last started observation that we received on the channel.
+	// if we get an checkin, we'll just update the existing observation
+	currentStartedObservation *observation
+	// version of the data that we wrote to redis.
+	// each observation we get, we'll update version. When we flush it to redis, we'll update lastWrittenVersion.
+	// This will keep us from writing to redis unless necessary
+	version            int64
+	lastWrittenVersion int64
+	observationsChan   chan *observation
+	stopChan           chan struct{}
+	doneStoppingChan   chan struct{}
+	drainChan          chan struct{}
+	doneDrainingChan   chan struct{}
 }
