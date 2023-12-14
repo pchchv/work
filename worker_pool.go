61861ac6c8f85efbe49671ef2d2292d39b4bb4b7
pchchv <ipchchv@gmail.com>
2023-12-14 09:39:24 +0300

    GenericMiddlewareHandler&NextMiddlewareFunc types

--- worker_pool.go
+++ worker_pool.go
@@ -17,3 +17,11 @@ type JobOptions struct {
 
 // GenericHandler is a job handler without any custom context.
 type GenericHandler func(*Job) error
+
+// GenericMiddlewareHandler is a middleware without any custom context.
+type GenericMiddlewareHandler func(*Job, NextMiddlewareFunc) error
+
+// NextMiddlewareFunc is a function type
+// (whose instances are named 'next')
+// that you call to advance to the next middleware.
+type NextMiddlewareFunc func() error
