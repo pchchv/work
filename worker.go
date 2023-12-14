33a3bda29922a3794237b7e13a055622a0b8d6f7
pchchv <ipchchv@gmail.com>
2023-12-14 09:50:29 +0300

    Add a worker type

--- worker.go
+++ worker.go
@@ -1,6 +1,29 @@
 package work
 
-import "math/rand"
+import (
+	"math/rand"
+	"reflect"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+type worker struct {
+	workerID         string
+	poolID           string
+	namespace        string
+	pool             *redis.Pool
+	jobTypes         map[string]*jobType
+	sleepBackoffs    []int64
+	middleware       []*middlewareHandler
+	contextType      reflect.Type
+	redisFetchScript *redis.Script
+	sampler          prioritySampler
+	*observer
+	stopChan         chan struct{}
+	doneStoppingChan chan struct{}
+	drainChan        chan struct{}
+	doneDrainingChan chan struct{}
+}
 
 // Default algorithm returns a fastly increasing unboundedly fashion backoff counter.
 func defaultBackoffCalculator(job *Job) int64 {
