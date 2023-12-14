4ac3fae47f6cf4522eca588c96770a8fe5497ed5
pchchv <ipchchv@gmail.com>
2023-12-14 09:34:41 +0300

    Add a JobOptions type

--- worker_pool.go
+++ worker_pool.go
@@ -5,3 +5,12 @@ package work
 //
 // The builtin backoff calculator provides an exponentially increasing wait function.
 type BackoffCalculator func(job *Job) int64
+
+// JobOptions can be passed to JobWithOptions.
+type JobOptions struct {
+	Priority       uint              // Priority from 1 to 10000
+	MaxFails       uint              // 1: send straight to dead (unless SkipDead)
+	SkipDead       bool              // If true, don't send failed jobs to the dead queue when retries are exhausted.
+	MaxConcurrency uint              // Max number of jobs to keep in flight (default is 0, meaning no max)
+	Backoff        BackoffCalculator // If not set, uses the default backoff algorithm
+}
