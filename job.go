640a705e50f11d26e0f8ac3252d78ef41ca14213
pchchv <ipchchv@gmail.com>
2023-12-14 09:19:36 +0300

    isIntKind&isUintKind&isFloatKind functions

--- job.go
+++ job.go
@@ -3,6 +3,7 @@ package work
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"reflect"
 )
 
@@ -97,4 +98,19 @@ func typecastError(jsonType, key string, v interface{}) error {
 
 func missingKeyError(jsonType, key string) error {
 	return fmt.Errorf("looking for a %s in job.Arg[%s] but key wasn't found", jsonType, key)
+}
+
+func isIntKind(v reflect.Value) bool {
+	k := v.Kind()
+	return k == reflect.Int || k == reflect.Int8 || k == reflect.Int16 || k == reflect.Int32 || k == reflect.Int64
+}
+
+func isUintKind(v reflect.Value) bool {
+	k := v.Kind()
+	return k == reflect.Uint || k == reflect.Uint8 || k == reflect.Uint16 || k == reflect.Uint32 || k == reflect.Uint64
+}
+
+func isFloatKind(v reflect.Value) bool {
+	k := v.Kind()
+	return k == reflect.Float32 || k == reflect.Float64
 }
