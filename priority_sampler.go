35a32d2a33da1443d7d43a20f8941c17afb83f58
pchchv <ipchchv@gmail.com>
2023-12-14 09:48:02 +0300

    Implement an add method

--- priority_sampler.go
+++ priority_sampler.go
@@ -15,3 +15,24 @@ type prioritySampler struct {
 	sum     uint
 	samples []sampleItem
 }
+
+func (s *prioritySampler) add(
+	priority uint,
+	redisJobs,
+	redisJobsInProg,
+	redisJobsPaused,
+	redisJobsLock,
+	redisJobsLockInfo,
+	redisJobsMaxConcurrency string) {
+	sample := sampleItem{
+		priority:                priority,
+		redisJobs:               redisJobs,
+		redisJobsInProg:         redisJobsInProg,
+		redisJobsPaused:         redisJobsPaused,
+		redisJobsLock:           redisJobsLock,
+		redisJobsLockInfo:       redisJobsLockInfo,
+		redisJobsMaxConcurrency: redisJobsMaxConcurrency,
+	}
+	s.samples = append(s.samples, sample)
+	s.sum += priority
+}
