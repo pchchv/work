8fac7747a48f2171b151a06613f335f5b59380d1
pchchv <ipchchv@gmail.com>
2023-12-14 09:44:41 +0300

    Implement a calcBackoff method

--- worker_pool.go
+++ worker_pool.go
@@ -46,3 +46,10 @@ type jobType struct {
 	GenericHandler GenericHandler
 	DynamicHandler reflect.Value
 }
+
+func (jt *jobType) calcBackoff(j *Job) int64 {
+	if jt.Backoff == nil {
+		return defaultBackoffCalculator(j)
+	}
+	return jt.Backoff(j)
+}
