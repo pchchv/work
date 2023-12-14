18253093b11455922191979d5bc1219e94da2c93
pchchv <ipchchv@gmail.com>
2023-12-14 09:44:20 +0300

    Implement a defaultBackoffCalculator function

--- worker.go
+++ worker.go
@@ -1,1 +1,9 @@
 package work
+
+import "math/rand"
+
+// Default algorithm returns a fastly increasing unboundedly fashion backoff counter.
+func defaultBackoffCalculator(job *Job) int64 {
+	fails := job.Fails
+	return (fails * fails * fails * fails) + 15 + (rand.Int63n(30) * (fails + 1))
+}
