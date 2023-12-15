6b04b01519e8148ff836d0cad6770c0d9c43a534
pchchv <ipchchv@gmail.com>
2023-12-15 19:18:42 +0300

    Implement an updateMiddlewareAndJobTypes method

--- worker.go
+++ worker.go
@@ -25,6 +25,24 @@ type worker struct {
 	doneDrainingChan chan struct{}
 }
 
+// note: can't be called while the thing is started
+func (w *worker) updateMiddlewareAndJobTypes(middleware []*middlewareHandler, jobTypes map[string]*jobType) {
+	w.middleware = middleware
+	sampler := prioritySampler{}
+	for _, jt := range jobTypes {
+		sampler.add(jt.Priority,
+			redisKeyJobs(w.namespace, jt.Name),
+			redisKeyJobsInProgress(w.namespace, w.poolID, jt.Name),
+			redisKeyJobsPaused(w.namespace, jt.Name),
+			redisKeyJobsLock(w.namespace, jt.Name),
+			redisKeyJobsLockInfo(w.namespace, jt.Name),
+			redisKeyJobsConcurrency(w.namespace, jt.Name))
+	}
+	w.sampler = sampler
+	w.jobTypes = jobTypes
+	w.redisFetchScript = redis.NewScript(len(jobTypes)*fetchKeysPerJobType, redisLuaFetchJob)
+}
+
 // Default algorithm returns a fastly increasing unboundedly fashion backoff counter.
 func defaultBackoffCalculator(job *Job) int64 {
 	fails := job.Fails
