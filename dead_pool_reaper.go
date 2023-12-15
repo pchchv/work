a17813c2e6578c3b926960e32715f938806c1e6e
pchchv <ipchchv@gmail.com>
2023-12-15 19:52:59 +0300

    Implement a findDeadPools method

--- dead_pool_reaper.go
+++ dead_pool_reaper.go
@@ -1,6 +1,7 @@
 package work
 
 import (
+	"strings"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
@@ -33,4 +34,47 @@ func newDeadPoolReaper(namespace string, pool *redis.Pool, curJobTypes []string)
 		stopChan:         make(chan struct{}),
 		doneStoppingChan: make(chan struct{}),
 	}
+}
+
+func (r *deadPoolReaper) findDeadPools() (map[string][]string, error) {
+	conn := r.pool.Get()
+	defer conn.Close()
+
+	workerPoolsKey := redisKeyWorkerPools(r.namespace)
+
+	workerPoolIDs, err := redis.Strings(conn.Do("SMEMBERS", workerPoolsKey))
+	if err != nil {
+		return nil, err
+	}
+
+	deadPools := map[string][]string{}
+	for _, workerPoolID := range workerPoolIDs {
+		heartbeatKey := redisKeyHeartbeat(r.namespace, workerPoolID)
+		heartbeatAt, err := redis.Int64(conn.Do("HGET", heartbeatKey, "heartbeat_at"))
+		if err == redis.ErrNil {
+			// heartbeat expired, save dead pool and use cur set of jobs from reaper
+			deadPools[workerPoolID] = []string{}
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		// Check that last heartbeat was long enough ago to consider the pool dead
+		if time.Unix(heartbeatAt, 0).Add(r.deadTime).After(time.Now()) {
+			continue
+		}
+
+		jobTypesList, err := redis.String(conn.Do("HGET", heartbeatKey, "job_names"))
+		if err == redis.ErrNil {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		deadPools[workerPoolID] = strings.Split(jobTypesList, ",")
+	}
+
+	return deadPools, nil
 }
