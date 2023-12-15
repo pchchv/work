642b3b84e1d8015b9fe222a586bcdda57aecf56b
pchchv <ipchchv@gmail.com>
2023-12-15 19:59:52 +0300

    Add a scheduledPeriodicJob type

--- periodic_enqueuer.go
+++ periodic_enqueuer.go
@@ -1,9 +1,19 @@
 package work
 
-import "github.com/robfig/cron/v3"
+import (
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
 
 type periodicJob struct {
 	spec     string
 	jobName  string
 	schedule cron.Schedule
+}
+
+type scheduledPeriodicJob struct {
+	scheduledAt      time.Time
+	scheduledAtEpoch int64
+	*periodicJob
 }
