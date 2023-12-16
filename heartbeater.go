466a1529f90b66afbc7088f04320dfe6aa5c454c
pchchv <ipchchv@gmail.com>
2023-12-16 16:49:11 +0300

    Add a newWorkerPoolHeartbeater func

--- heartbeater.go
+++ heartbeater.go
@@ -1,10 +1,15 @@
 package work
 
 import (
+	"os"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
+
+const beatPeriod = 5 * time.Second
 
 type workerPoolHeartbeater struct {
 	workerPoolID     string
@@ -19,4 +24,42 @@ type workerPoolHeartbeater struct {
 	workerIDs        string
 	stopChan         chan struct{}
 	doneStoppingChan chan struct{}
+}
+
+func newWorkerPoolHeartbeater(
+	namespace string,
+	pool *redis.Pool,
+	workerPoolID string,
+	jobTypes map[string]*jobType,
+	concurrency uint,
+	workerIDs []string) *workerPoolHeartbeater {
+	h := &workerPoolHeartbeater{
+		workerPoolID:     workerPoolID,
+		namespace:        namespace,
+		pool:             pool,
+		beatPeriod:       beatPeriod,
+		concurrency:      concurrency,
+		stopChan:         make(chan struct{}),
+		doneStoppingChan: make(chan struct{}),
+	}
+
+	jobNames := make([]string, 0, len(jobTypes))
+	for k := range jobTypes {
+		jobNames = append(jobNames, k)
+	}
+	sort.Strings(jobNames)
+	h.jobNames = strings.Join(jobNames, ",")
+
+	sort.Strings(workerIDs)
+	h.workerIDs = strings.Join(workerIDs, ",")
+
+	h.pid = os.Getpid()
+	host, err := os.Hostname()
+	if err != nil {
+		logError("heartbeat.hostname", err)
+		host = "hostname_errored"
+	}
+
+	h.hostname = host
+	return h
 }
