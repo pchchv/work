97c9be1e17c86facbd755f7727e6cac2cecddccb
pchchv <ipchchv@gmail.com>
2023-12-17 14:14:31 +0300

    Implement a NewEnqueuer function

--- enqueue.go
+++ enqueue.go
@@ -16,3 +16,19 @@ type Enqueuer struct {
 	enqueueUniqueInScript *redis.Script
 	mtx                   sync.RWMutex
 }
+
+// NewEnqueuer creates a new enqueuer with
+// the specified Redis namespace and Redis pool.
+func NewEnqueuer(namespace string, pool *redis.Pool) *Enqueuer {
+	if pool == nil {
+		panic("NewEnqueuer needs a non-nil *redis.Pool")
+	}
+	return &Enqueuer{
+		Namespace:             namespace,
+		Pool:                  pool,
+		queuePrefix:           redisKeyJobsPrefix(namespace),
+		knownJobs:             make(map[string]int64),
+		enqueueUniqueScript:   redis.NewScript(2, redisLuaEnqueueUnique),
+		enqueueUniqueInScript: redis.NewScript(2, redisLuaEnqueueUniqueIn),
+	}
+}
