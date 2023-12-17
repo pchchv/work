918734700363e6db83f56648101bc399cbdee379
pchchv <ipchchv@gmail.com>
2023-12-17 14:32:14 +0300

    Add a WorkerPoolHeartbeat type

--- client.go
+++ client.go
@@ -5,3 +5,16 @@ type ScheduledJob struct {
 	RunAt int64 `json:"run_at"`
 	*Job
 }
+
+// WorkerPoolHeartbeat represents the heartbeat from a worker pool.
+// WorkerPool's write a heartbeat every 5 seconds so we know they're alive and includes config information.
+type WorkerPoolHeartbeat struct {
+	WorkerPoolID string   `json:"worker_pool_id"`
+	StartedAt    int64    `json:"started_at"`
+	HeartbeatAt  int64    `json:"heartbeat_at"`
+	JobNames     []string `json:"job_names"`
+	Concurrency  uint     `json:"concurrency"`
+	Host         string   `json:"host"`
+	Pid          int      `json:"pid"`
+	WorkerIDs    []string `json:"worker_ids"`
+}
