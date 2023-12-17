0c77a4ed1838994ddf3c8c72cce372131374f297
pchchv <ipchchv@gmail.com>
2023-12-17 14:34:14 +0300

    Add a Queue type

--- client.go
+++ client.go
@@ -33,3 +33,12 @@ type WorkerObservation struct {
 	Checkin   string `json:"checkin"`
 	CheckinAt int64  `json:"checkin_at"`
 }
+
+// Queue represents a queue that holds jobs with the same name.
+// It indicates their name, count, and latency (in seconds).
+// Latency is a measurement of how long ago the next job to be processed was enqueued.
+type Queue struct {
+	JobName string `json:"job_name"`
+	Count   int64  `json:"count"`
+	Latency int64  `json:"latency"`
+}
