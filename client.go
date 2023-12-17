4366ac608a0ff8a9bed699f54e65958879b24db3
pchchv <ipchchv@gmail.com>
2023-12-17 14:32:55 +0300

    Add a WorkerObservation type

--- client.go
+++ client.go
@@ -18,3 +18,18 @@ type WorkerPoolHeartbeat struct {
 	Pid          int      `json:"pid"`
 	WorkerIDs    []string `json:"worker_ids"`
 }
+
+// WorkerObservation represents the latest observation taken from a worker.
+// The observation indicates whether the worker is busy processing a job,
+// and if so, information about that job.
+type WorkerObservation struct {
+	WorkerID string `json:"worker_id"`
+	IsBusy   bool   `json:"is_busy"`
+	// If IsBusy:
+	JobName   string `json:"job_name"`
+	JobID     string `json:"job_id"`
+	StartedAt int64  `json:"started_at"`
+	ArgsJSON  string `json:"args_json"`
+	Checkin   string `json:"checkin"`
+	CheckinAt int64  `json:"checkin_at"`
+}
