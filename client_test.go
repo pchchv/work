067b24b69ef7ac5588ed819830e205eed1847af8
pchchv <ipchchv@gmail.com>
2023-12-19 19:45:46 +0300

    Implement a client worker pool heartbeats tests

--- client_test.go
+++ client_test.go
@@ -1,3 +1,62 @@
 package work
 
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
 type TestContext struct{}
+
+func TestClientWorkerPoolHeartbeats(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+
+	wp := NewWorkerPool(TestContext{}, 10, ns, pool)
+	wp.Job("wat", func(job *Job) error { return nil })
+	wp.Job("bob", func(job *Job) error { return nil })
+	wp.Start()
+
+	wp2 := NewWorkerPool(TestContext{}, 11, ns, pool)
+	wp2.Job("foo", func(job *Job) error { return nil })
+	wp2.Job("bar", func(job *Job) error { return nil })
+	wp2.Start()
+
+	time.Sleep(20 * time.Millisecond)
+
+	client := NewClient(ns, pool)
+
+	hbs, err := client.WorkerPoolHeartbeats()
+	assert.NoError(t, err)
+	assert.EqualValues(t, 2, len(hbs))
+	if len(hbs) == 2 {
+		var hbwp, hbwp2 *WorkerPoolHeartbeat
+
+		if wp.workerPoolID == hbs[0].WorkerPoolID {
+			hbwp = hbs[0]
+			hbwp2 = hbs[1]
+		} else {
+			hbwp = hbs[1]
+			hbwp2 = hbs[0]
+		}
+
+		assert.Equal(t, wp.workerPoolID, hbwp.WorkerPoolID)
+		assert.EqualValues(t, uint(10), hbwp.Concurrency)
+		assert.Equal(t, []string{"bob", "wat"}, hbwp.JobNames)
+		assert.Equal(t, wp.workerIDs(), hbwp.WorkerIDs)
+
+		assert.Equal(t, wp2.workerPoolID, hbwp2.WorkerPoolID)
+		assert.EqualValues(t, uint(11), hbwp2.Concurrency)
+		assert.Equal(t, []string{"bar", "foo"}, hbwp2.JobNames)
+		assert.Equal(t, wp2.workerIDs(), hbwp2.WorkerIDs)
+	}
+
+	wp.Stop()
+	wp2.Stop()
+
+	hbs, err = client.WorkerPoolHeartbeats()
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(hbs))
+}
