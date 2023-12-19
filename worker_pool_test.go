2c223482ef00b1ee141863abce1e1e3e50c5c456
pchchv <ipchchv@gmail.com>
2023-12-19 19:43:04 +0300

    Implement a workers pool run single threaded tests

--- worker_pool_test.go
+++ worker_pool_test.go
@@ -121,6 +121,49 @@ func TestWorkerPoolValidations(t *testing.T) {
 	}()
 }
 
+func TestWorkersPoolRunSingleThreaded(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	job1 := "job1"
+	numJobs, concurrency, sleepTime := 5, 5, 2
+	wp := setupTestWorkerPool(pool, ns, job1, concurrency, JobOptions{Priority: 1, MaxConcurrency: 1})
+	wp.Start()
+	// enqueue some jobs
+	enqueuer := NewEnqueuer(ns, pool)
+	for i := 0; i < numJobs; i++ {
+		_, err := enqueuer.Enqueue(job1, Q{"sleep": sleepTime})
+		assert.Nil(t, err)
+	}
+
+	// make sure we've enough jobs queued up to make an interesting test
+	jobsQueued := listSize(pool, redisKeyJobs(ns, job1))
+	assert.True(t, jobsQueued >= 3, "should be at least 3 jobs queued up, but only found %v", jobsQueued)
+
+	// now make sure the during the duration of job execution there is never > 1 job in flight
+	start := time.Now()
+	totalRuntime := time.Duration(sleepTime*numJobs) * time.Millisecond
+	time.Sleep(10 * time.Millisecond)
+	for time.Since(start) < totalRuntime {
+		// jobs in progress, lock count for the job and lock info for the pool should never exceed 1
+		jobsInProgress := listSize(pool, redisKeyJobsInProgress(ns, wp.workerPoolID, job1))
+		assert.True(t, jobsInProgress <= 1, "jobsInProgress should never exceed 1: actual=%d", jobsInProgress)
+
+		jobLockCount := getInt64(pool, redisKeyJobsLock(ns, job1))
+		assert.True(t, jobLockCount <= 1, "global lock count for job should never exceed 1, got: %v", jobLockCount)
+		wpLockCount := hgetInt64(pool, redisKeyJobsLockInfo(ns, job1), wp.workerPoolID)
+		assert.True(t, wpLockCount <= 1, "lock count for the worker pool should never exceed 1: actual=%v", wpLockCount)
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+	}
+	wp.Drain()
+	wp.Stop()
+
+	// At this point it should all be empty.
+	assert.EqualValues(t, 0, listSize(pool, redisKeyJobs(ns, job1)))
+	assert.EqualValues(t, 0, listSize(pool, redisKeyJobsInProgress(ns, wp.workerPoolID, job1)))
+	assert.EqualValues(t, 0, getInt64(pool, redisKeyJobsLock(ns, job1)))
+	assert.EqualValues(t, 0, hgetInt64(pool, redisKeyJobsLockInfo(ns, job1), wp.workerPoolID))
+}
+
 func setupTestWorkerPool(pool *redis.Pool, namespace, jobName string, concurrency int, jobOpts JobOptions) *WorkerPool {
 	deleteQueue(pool, namespace, jobName)
 	deleteRetryAndDead(pool, namespace)
