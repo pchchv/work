e17a4d1bfd518b7ab3e3db959df62d293819fc08
pchchv <ipchchv@gmail.com>
2023-12-19 19:46:33 +0300

    Implement a client worker observations tests

--- client_test.go
+++ client_test.go
@@ -60,3 +60,62 @@ func TestClientWorkerPoolHeartbeats(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, len(hbs))
 }
+
+func TestClientWorkerObservations(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+
+	enqueuer := NewEnqueuer(ns, pool)
+	_, err := enqueuer.Enqueue("wat", Q{"a": 1, "b": 2})
+	assert.Nil(t, err)
+	_, err = enqueuer.Enqueue("foo", Q{"a": 3, "b": 4})
+	assert.Nil(t, err)
+
+	wp := NewWorkerPool(TestContext{}, 10, ns, pool)
+	wp.Job("wat", func(job *Job) error {
+		time.Sleep(50 * time.Millisecond)
+		return nil
+	})
+	wp.Job("foo", func(job *Job) error {
+		time.Sleep(50 * time.Millisecond)
+		return nil
+	})
+	wp.Start()
+
+	time.Sleep(10 * time.Millisecond)
+
+	client := NewClient(ns, pool)
+	observations, err := client.WorkerObservations()
+	assert.NoError(t, err)
+	assert.Equal(t, 10, len(observations))
+
+	watCount := 0
+	fooCount := 0
+	for _, ob := range observations {
+		if ob.JobName == "foo" {
+			fooCount++
+			assert.True(t, ob.IsBusy)
+			assert.Equal(t, `{"a":3,"b":4}`, ob.ArgsJSON)
+			assert.True(t, (nowEpochSeconds()-ob.StartedAt) <= 3)
+			assert.True(t, ob.JobID != "")
+		} else if ob.JobName == "wat" {
+			watCount++
+			assert.True(t, ob.IsBusy)
+			assert.Equal(t, `{"a":1,"b":2}`, ob.ArgsJSON)
+			assert.True(t, (nowEpochSeconds()-ob.StartedAt) <= 3)
+			assert.True(t, ob.JobID != "")
+		} else {
+			assert.False(t, ob.IsBusy)
+		}
+		assert.True(t, ob.WorkerID != "")
+	}
+	assert.Equal(t, 1, watCount)
+	assert.Equal(t, 1, fooCount)
+
+	wp.Stop()
+
+	observations, err = client.WorkerObservations()
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(observations))
+}
