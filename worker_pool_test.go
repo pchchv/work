b37e6ac92f09b98ec1607bd3b67a4e59d45610a6
pchchv <ipchchv@gmail.com>
2023-12-19 19:36:07 +0300

    Implement a worker pool handler validations test

--- worker_pool_test.go
+++ worker_pool_test.go
@@ -3,6 +3,7 @@ package work
 import (
 	"bytes"
 	"reflect"
+	"testing"
 )
 
 var tstCtxType = reflect.TypeOf(tstCtx{})
@@ -14,4 +15,28 @@ type tstCtx struct {
 
 func (c *tstCtx) record(s string) {
 	_, _ = c.WriteString(s)
+}
+
+func TestWorkerPoolHandlerValidations(t *testing.T) {
+	var cases = []struct {
+		fn   interface{}
+		good bool
+	}{
+		{func(j *Job) error { return nil }, true},
+		{func(c *tstCtx, j *Job) error { return nil }, true},
+		{func(c *tstCtx, j *Job) {}, false},
+		{func(c *tstCtx, j *Job) string { return "" }, false},
+		{func(c *tstCtx, j *Job) (error, string) { return nil, "" }, false},
+		{func(c *tstCtx) error { return nil }, false},
+		{func(c tstCtx, j *Job) error { return nil }, false},
+		{func() error { return nil }, false},
+		{func(c *tstCtx, j *Job, wat string) error { return nil }, false},
+	}
+
+	for i, testCase := range cases {
+		r := isValidHandlerType(tstCtxType, reflect.ValueOf(testCase.fn))
+		if testCase.good != r {
+			t.Errorf("idx %d: should return %v but returned %v", i, testCase.good, r)
+		}
+	}
 }
