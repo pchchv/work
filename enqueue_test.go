cf5fa92968011b91556222cc1276ceb5c983a618
pchchv <ipchchv@gmail.com>
2023-12-20 16:57:28 +0300

    Implement an Enqueue in tests

--- enqueue_test.go
+++ enqueue_test.go
@@ -48,3 +48,50 @@ func TestEnqueue(t *testing.T) {
 	assert.Nil(t, err)
 	assert.EqualValues(t, 2, listSize(pool, redisKeyJobs(ns, "wat")))
 }
+
+func TestEnqueueIn(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+	enqueuer := NewEnqueuer(ns, pool)
+
+	// Set to expired value to make sure we update the set of known jobs
+	enqueuer.knownJobs["wat"] = 4
+
+	job, err := enqueuer.EnqueueIn("wat", 300, Q{"a": 1, "b": "cool"})
+	assert.Nil(t, err)
+	if assert.NotNil(t, job) {
+		assert.Equal(t, "wat", job.Name)
+		assert.True(t, len(job.ID) > 10)                        // Something is in it
+		assert.True(t, job.EnqueuedAt > (time.Now().Unix()-10)) // Within 10 seconds
+		assert.True(t, job.EnqueuedAt < (time.Now().Unix()+10)) // Within 10 seconds
+		assert.Equal(t, "cool", job.ArgString("b"))
+		assert.EqualValues(t, 1, job.ArgInt64("a"))
+		assert.NoError(t, job.ArgError())
+		assert.EqualValues(t, job.EnqueuedAt+300, job.RunAt)
+	}
+
+	// Make sure "wat" is in the known jobs
+	assert.EqualValues(t, []string{"wat"}, knownJobs(pool, redisKeyKnownJobs(ns)))
+
+	// Make sure the cache is set
+	expiresAt := enqueuer.knownJobs["wat"]
+	assert.True(t, expiresAt > (time.Now().Unix()+290))
+
+	// Make sure the length of the scheduled job queue is 1
+	assert.EqualValues(t, 1, zsetSize(pool, redisKeyScheduled(ns)))
+
+	// Get the job
+	score, j := jobOnZset(pool, redisKeyScheduled(ns))
+
+	assert.True(t, score > time.Now().Unix()+290)
+	assert.True(t, score <= time.Now().Unix()+300)
+
+	assert.Equal(t, "wat", j.Name)
+	assert.True(t, len(j.ID) > 10)                        // Something is in it
+	assert.True(t, j.EnqueuedAt > (time.Now().Unix()-10)) // Within 10 seconds
+	assert.True(t, j.EnqueuedAt < (time.Now().Unix()+10)) // Within 10 seconds
+	assert.Equal(t, "cool", j.ArgString("b"))
+	assert.EqualValues(t, 1, j.ArgInt64("a"))
+	assert.NoError(t, j.ArgError())
+}
