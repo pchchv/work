423520e4409a56c89eda142bdf07b831e46b4e1d
pchchv <ipchchv@gmail.com>
2023-12-20 16:59:03 +0300

    Implement an enqueue unique tests

--- enqueue_test.go
+++ enqueue_test.go
@@ -1,6 +1,8 @@
 package work
 
 import (
+	"errors"
+	"sync"
 	"testing"
 	"time"
 
@@ -94,4 +96,80 @@ func TestEnqueueIn(t *testing.T) {
 	assert.Equal(t, "cool", j.ArgString("b"))
 	assert.EqualValues(t, 1, j.ArgInt64("a"))
 	assert.NoError(t, j.ArgError())
+}
+
+func TestEnqueueUnique(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+	enqueuer := NewEnqueuer(ns, pool)
+	var mutex = &sync.Mutex{}
+	job, err := enqueuer.EnqueueUnique("wat", Q{"a": 1, "b": "cool"})
+	assert.NoError(t, err)
+	if assert.NotNil(t, job) {
+		assert.Equal(t, "wat", job.Name)
+		assert.True(t, len(job.ID) > 10)                        // Something is in it
+		assert.True(t, job.EnqueuedAt > (time.Now().Unix()-10)) // Within 10 seconds
+		assert.True(t, job.EnqueuedAt < (time.Now().Unix()+10)) // Within 10 seconds
+		assert.Equal(t, "cool", job.ArgString("b"))
+		assert.EqualValues(t, 1, job.ArgInt64("a"))
+		assert.NoError(t, job.ArgError())
+	}
+
+	job, err = enqueuer.EnqueueUnique("wat", Q{"a": 1, "b": "cool"})
+	assert.NoError(t, err)
+	assert.Nil(t, job)
+
+	job, err = enqueuer.EnqueueUnique("wat", Q{"a": 1, "b": "coolio"})
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
+
+	job, err = enqueuer.EnqueueUnique("wat", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
+
+	job, err = enqueuer.EnqueueUnique("wat", nil)
+	assert.NoError(t, err)
+	assert.Nil(t, job)
+
+	job, err = enqueuer.EnqueueUnique("taw", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
+
+	// Process the queues. Ensure the right number of jobs were processed
+	var wats, taws int64
+	wp := NewWorkerPool(TestContext{}, 3, ns, pool)
+	wp.JobWithOptions("wat", JobOptions{Priority: 1, MaxFails: 1}, func(job *Job) error {
+		mutex.Lock()
+		wats++
+		mutex.Unlock()
+		return nil
+	})
+	wp.JobWithOptions("taw", JobOptions{Priority: 1, MaxFails: 1}, func(job *Job) error {
+		mutex.Lock()
+		taws++
+		mutex.Unlock()
+		return errors.New("ohno")
+	})
+	wp.Start()
+	wp.Drain()
+	wp.Stop()
+
+	assert.EqualValues(t, 3, wats)
+	assert.EqualValues(t, 1, taws)
+
+	// Enqueue again. Ensure we can.
+	job, err = enqueuer.EnqueueUnique("wat", Q{"a": 1, "b": "cool"})
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
+
+	job, err = enqueuer.EnqueueUnique("wat", Q{"a": 1, "b": "coolio"})
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
+
+	// Even though taw resulted in an error, we should still be able to re-queue it.
+	// This could result in multiple taws enqueued at the same time in a production system.
+	job, err = enqueuer.EnqueueUnique("taw", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, job)
 }
