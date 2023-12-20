ac80fc48ac5f79433175aa0a4e7ef79a0579cdb3
pchchv <ipchchv@gmail.com>
2023-12-20 17:03:21 +0300

    Implement a dead pool reaper no heartbeat tests

--- dead_pool_reaper_test.go
+++ dead_pool_reaper_test.go
@@ -90,3 +90,90 @@ func TestDeadPoolReaper(t *testing.T) {
 	v, _ := conn.Do("HGET", redisKeyJobsLockInfo(ns, "type1"), "2")
 	assert.Nil(t, v)
 }
+
+func TestDeadPoolReaperNoHeartbeat(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+
+	conn := pool.Get()
+	defer conn.Close()
+
+	workerPoolsKey := redisKeyWorkerPools(ns)
+
+	// Create redis data
+	var err error
+	cleanKeyspace(ns, pool)
+	err = conn.Send("SADD", workerPoolsKey, "1")
+	assert.NoError(t, err)
+	err = conn.Send("SADD", workerPoolsKey, "2")
+	assert.NoError(t, err)
+	err = conn.Send("SADD", workerPoolsKey, "3")
+	assert.NoError(t, err)
+	// stale lock info
+	err = conn.Send("SET", redisKeyJobsLock(ns, "type1"), 3)
+	assert.NoError(t, err)
+	err = conn.Send("HSET", redisKeyJobsLockInfo(ns, "type1"), "1", 1)
+	assert.NoError(t, err)
+	err = conn.Send("HSET", redisKeyJobsLockInfo(ns, "type1"), "2", 1)
+	assert.NoError(t, err)
+	err = conn.Send("HSET", redisKeyJobsLockInfo(ns, "type1"), "3", 1)
+	assert.NoError(t, err)
+	err = conn.Flush()
+	assert.NoError(t, err)
+
+	// make sure test data was created
+	numPools, err := redis.Int(conn.Do("scard", workerPoolsKey))
+	assert.NoError(t, err)
+	assert.EqualValues(t, 3, numPools)
+
+	// Test getting dead pool ids
+	reaper := newDeadPoolReaper(ns, pool, []string{"type1"})
+	deadPools, err := reaper.findDeadPools()
+	assert.NoError(t, err)
+	assert.Equal(t, map[string][]string{"1": {}, "2": {}, "3": {}}, deadPools)
+
+	// Test requeueing jobs
+	_, err = conn.Do("lpush", redisKeyJobsInProgress(ns, "2", "type1"), "foo")
+	assert.NoError(t, err)
+
+	// Ensure 0 jobs in jobs queue
+	jobsCount, err := redis.Int(conn.Do("llen", redisKeyJobs(ns, "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, jobsCount)
+
+	// Ensure 1 job in inprogress queue
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "2", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+
+	// Ensure dead worker pools still in the set
+	jobsCount, err = redis.Int(conn.Do("scard", redisKeyWorkerPools(ns)))
+	assert.NoError(t, err)
+	assert.Equal(t, 3, jobsCount)
+
+	// Reap
+	err = reaper.reap()
+	assert.NoError(t, err)
+
+	// Ensure jobs queue was not altered
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobs(ns, "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, jobsCount)
+
+	// Ensure inprogress queue was not altered
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "2", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+
+	// Ensure dead worker pools were removed from the set
+	jobsCount, err = redis.Int(conn.Do("scard", redisKeyWorkerPools(ns)))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, jobsCount)
+
+	// Stale lock info was cleaned up using reap.curJobTypes
+	assert.EqualValues(t, 0, getInt64(pool, redisKeyJobsLock(ns, "type1")))
+	for _, poolID := range []string{"1", "2", "3"} {
+		v, _ := conn.Do("HGET", redisKeyJobsLockInfo(ns, "type1"), poolID)
+		assert.Nil(t, v)
+	}
+}
