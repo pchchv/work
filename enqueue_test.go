821b2bbfba2c33ab977ec3922dba3a1cbf207e6a
pchchv <ipchchv@gmail.com>
2023-12-20 16:57:04 +0300

    Implement an Enqueue tests

---
+++ enqueue_test.go
@@ -0,0 +1,50 @@
+package work
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnqueue(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+	enqueuer := NewEnqueuer(ns, pool)
+	job, err := enqueuer.Enqueue("wat", Q{"a": 1, "b": "cool"})
+	assert.Nil(t, err)
+	assert.Equal(t, "wat", job.Name)
+	assert.True(t, len(job.ID) > 10)                        // Something is in it
+	assert.True(t, job.EnqueuedAt > (time.Now().Unix()-10)) // Within 10 seconds
+	assert.True(t, job.EnqueuedAt < (time.Now().Unix()+10)) // Within 10 seconds
+	assert.Equal(t, "cool", job.ArgString("b"))
+	assert.EqualValues(t, 1, job.ArgInt64("a"))
+	assert.NoError(t, job.ArgError())
+
+	// Make sure "wat" is in the known jobs
+	assert.EqualValues(t, []string{"wat"}, knownJobs(pool, redisKeyKnownJobs(ns)))
+
+	// Make sure the cache is set
+	expiresAt := enqueuer.knownJobs["wat"]
+	assert.True(t, expiresAt > (time.Now().Unix()+290))
+
+	// Make sure the length of the queue is 1
+	assert.EqualValues(t, 1, listSize(pool, redisKeyJobs(ns, "wat")))
+
+	// Get the job
+	j := jobOnQueue(pool, redisKeyJobs(ns, "wat"))
+	assert.Equal(t, "wat", j.Name)
+	assert.True(t, len(j.ID) > 10)                        // Something is in it
+	assert.True(t, j.EnqueuedAt > (time.Now().Unix()-10)) // Within 10 seconds
+	assert.True(t, j.EnqueuedAt < (time.Now().Unix()+10)) // Within 10 seconds
+	assert.Equal(t, "cool", j.ArgString("b"))
+	assert.EqualValues(t, 1, j.ArgInt64("a"))
+	assert.NoError(t, j.ArgError())
+
+	// Now enqueue another job, make sure that we can enqueue multiple
+	_, err = enqueuer.Enqueue("wat", Q{"a": 1, "b": "cool"})
+	_, err = enqueuer.Enqueue("wat", Q{"a": 1, "b": "cool"})
+	assert.Nil(t, err)
+	assert.EqualValues(t, 2, listSize(pool, redisKeyJobs(ns, "wat")))
+}
