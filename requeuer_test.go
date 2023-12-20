f9109205680c8531120ed725543aa41d67c71ab9
pchchv <ipchchv@gmail.com>
2023-12-20 16:43:48 +0300

    Implement an unknown requeue tests

--- requeuer_test.go
+++ requeuer_test.go
@@ -47,3 +47,34 @@ func TestRequeue(t *testing.T) {
 	// On boundary conditions with the VM, nowEpochSeconds() might be 1 or 2 secs ahead of EnqueuedAt
 	assert.True(t, (j.EnqueuedAt+2) >= nowEpochSeconds())
 }
+
+func TestRequeueUnknown(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+
+	tMock := nowEpochSeconds() - 10
+	setNowEpochSecondsMock(tMock)
+	defer resetNowEpochSecondsMock()
+
+	enqueuer := NewEnqueuer(ns, pool)
+	_, err := enqueuer.EnqueueIn("wat", -9, nil)
+	assert.NoError(t, err)
+
+	nowish := nowEpochSeconds()
+	setNowEpochSecondsMock(nowish)
+
+	re := newRequeuer(ns, pool, redisKeyScheduled(ns), []string{"bar"})
+	re.start()
+	re.drain()
+	re.stop()
+
+	assert.EqualValues(t, 0, zsetSize(pool, redisKeyScheduled(ns)))
+	assert.EqualValues(t, 1, zsetSize(pool, redisKeyDead(ns)))
+
+	rank, job := jobOnZset(pool, redisKeyDead(ns))
+
+	assert.Equal(t, nowish, rank)
+	assert.Equal(t, nowish, job.FailedAt)
+	assert.Equal(t, "unknown job when requeueing", job.LastErr)
+}
