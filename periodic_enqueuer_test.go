75ccb1254b61059932928c1e608d733e1cf284ae
pchchv <ipchchv@gmail.com>
2023-12-20 16:48:10 +0300

    Implement an appendPeriodicJob function

--- periodic_enqueuer_test.go
+++ periodic_enqueuer_test.go
@@ -1,6 +1,10 @@
 package work
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/robfig/cron/v3"
+)
 
 func TestPeriodicEnqueuerSpawn(t *testing.T) {
 	pool := newTestPool(":6379")
@@ -10,4 +14,16 @@ func TestPeriodicEnqueuerSpawn(t *testing.T) {
 	pe := newPeriodicEnqueuer(ns, pool, nil)
 	pe.start()
 	pe.stop()
+}
+
+func appendPeriodicJob(pjs []*periodicJob, spec, jobName string) []*periodicJob {
+	p := cron.NewParser(cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor)
+
+	sched, err := p.Parse(spec)
+	if err != nil {
+		panic(err)
+	}
+
+	pj := &periodicJob{jobName: jobName, spec: spec, schedule: sched}
+	return append(pjs, pj)
 }
