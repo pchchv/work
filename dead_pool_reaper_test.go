2fd4326314f97e97bf69c180768b5f1b41d04c25
pchchv <ipchchv@gmail.com>
2023-12-20 17:03:52 +0300

    Implement a dead pool reaper no job types tests

--- dead_pool_reaper_test.go
+++ dead_pool_reaper_test.go
@@ -177,3 +177,79 @@ func TestDeadPoolReaperNoHeartbeat(t *testing.T) {
 		assert.Nil(t, v)
 	}
 }
+
+func TestDeadPoolReaperNoJobTypes(t *testing.T) {
+	pool := newTestPool(":6379")
+	ns := "work"
+	cleanKeyspace(ns, pool)
+
+	conn := pool.Get()
+	defer conn.Close()
+
+	workerPoolsKey := redisKeyWorkerPools(ns)
+
+	// Create redis data
+	var err error
+	err = conn.Send("SADD", workerPoolsKey, "1")
+	assert.NoError(t, err)
+	err = conn.Send("SADD", workerPoolsKey, "2")
+	assert.NoError(t, err)
+
+	err = conn.Send("HMSET", redisKeyHeartbeat(ns, "1"),
+		"heartbeat_at", time.Now().Add(-1*time.Hour).Unix(),
+	)
+	assert.NoError(t, err)
+
+	err = conn.Send("HMSET", redisKeyHeartbeat(ns, "2"),
+		"heartbeat_at", time.Now().Add(-1*time.Hour).Unix(),
+		"job_names", "type1,type2",
+	)
+	assert.NoError(t, err)
+
+	err = conn.Flush()
+	assert.NoError(t, err)
+
+	// Test getting dead pool
+	reaper := newDeadPoolReaper(ns, pool, []string{})
+	deadPools, err := reaper.findDeadPools()
+	assert.NoError(t, err)
+	assert.Equal(t, map[string][]string{"2": {"type1", "type2"}}, deadPools)
+
+	// Test requeueing jobs
+	_, err = conn.Do("lpush", redisKeyJobsInProgress(ns, "1", "type1"), "foo")
+	assert.NoError(t, err)
+	_, err = conn.Do("lpush", redisKeyJobsInProgress(ns, "2", "type1"), "foo")
+	assert.NoError(t, err)
+
+	// Ensure 0 jobs in jobs queue
+	jobsCount, err := redis.Int(conn.Do("llen", redisKeyJobs(ns, "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, jobsCount)
+
+	// Ensure 1 job in inprogress queue for each job
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "1", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "2", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+
+	// Reap. Ensure job 2 is requeued but not job 1
+	err = reaper.reap()
+	assert.NoError(t, err)
+
+	// Ensure 1 jobs in jobs queue
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobs(ns, "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+
+	// Ensure 1 job in inprogress queue for 1
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "1", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, jobsCount)
+
+	// Ensure 0 jobs in inprogress queue for 2
+	jobsCount, err = redis.Int(conn.Do("llen", redisKeyJobsInProgress(ns, "2", "type1")))
+	assert.NoError(t, err)
+	assert.Equal(t, 0, jobsCount)
+}
